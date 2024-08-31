e0363950b1a8f39df611496bb17bf73de3a48919
Amisha Jethmalani <99468253+Amishaj25@users.noreply.github.com>
2024-09-01 02:03:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,3 +38,27 @@ else:
     string = "This image most likely is: "+class_names[np.argmax(predictions)]
     st.success(string)
 
+#streamlit run app.py
+# Save model architecture to JSON
+model_json = model.to_json()
+with open('model.json', 'w') as json_file:
+    json_file.write(model_json)
+
+# Save model weights to HDF5
+model.save_weights('model_weights.h5')
+from tensorflow.keras.models import model_from_json
+
+# Load the model architecture from JSON
+with open('model.json', 'r') as json_file:
+    model_json = json_file.read()
+loaded_model = model_from_json(model_json)
+
+# Load the model weights
+loaded_model.load_weights('model_weights.h5')
+export_path = "C://Users//amish//Desktop"
+import keras
+import tensorflow as tf
+from tensorflow.keras import models , layers
+# Save the model as a SavedModel
+tf.saved_model.save(model, export_path)
+
